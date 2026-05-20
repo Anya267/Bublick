d587726e42583f2921e43492a510b05df413f803
Holik Tany <tatanagolik36@gmail.com>
2026-05-21 01:49:37 +0300

    улучшила сообщения и калькулятор чётности

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-while True :
-    a = input ("Enter combination of 8 bits(1 or 0)")
-    if len(a) == 8 and a.count("0") + a.count("1") == 8:
-        if a.count("1") %2 == 0:
-            print("parity bit = 0")
-        else :
-            print("parity bit = 1")
-    else :
-        print("Error,plise enter combination of 8 bits 1 or 0")
-    if len(a)==0 :
+while True:
+    a = input("Enter combination of 8 bits (1 or 0) or 'exit' to quit: ")
+
+    if a.lower() == 'exit':
+        print("Goodbye!")
         break
 
+    if len(a) == 8 and a.count("0") + a.count("1") == 8:
+        ones_count = a.count("1")
+        if ones_count % 2 == 0:
+            print(f"Parity bit = 0 (even - {ones_count} ones)")
+        else:
+            print(f"Parity bit = 1 (odd - {ones_count} ones)")
+    else:
+        print("Error: Please enter exactly 8 bits (only 0 and 1)")
